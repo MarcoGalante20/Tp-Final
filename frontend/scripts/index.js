7eb80a003deb7985bb237d06327d4b082dba05bb
Juan-Dionesalvi <jdionesalvi@fi.uba.ar>
2025-07-11 16:31:01 -0300

    Preparado para hacer requests con filtros

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -39,4 +39,101 @@ async function cargarRelojes() {
     });
 }
 
-cargarRelojes()
+function estandarizarData(data) {
+    [data.precio, data.diametro, data.resistencia_agua].forEach((elemento) => {
+        if ((elemento.min === "") || (elemento.min < 0)) {
+            elemento.min = 0;
+        }
+        if (elemento.max === "") {
+            elemento.max = null;
+        }
+    })
+    return data;
+}
+
+function escucharFiltrado() {
+    const selectMarcas = document.getElementById("selectMarcas");
+    const seleccionadosMarcas = Array.from(selectMarcas.options)
+        .filter(opt => opt.selected)
+        .map(opt => opt.value);
+
+    //console.log("Marcas seleccionadas:", seleccionadosMarcas);
+
+
+    const minPrecio = document.getElementById("minPrecio");
+    const maxPrecio = document.getElementById("maxPrecio");
+    //console.log("Precio: min: ", minPrecio.value, ", max: ", maxPrecio.value);
+
+
+    const selectSexo = document.getElementById("selectSexo");
+    //console.log("Sexo: ", selectSexo.value);
+
+
+    const minDiametro = document.getElementById("minDiametro");
+    const maxDiametro = document.getElementById("maxDiametro");
+    //console.log("Diametro: min: ", minDiametro.value, ", max: ", maxDiametro.value);
+
+
+    const selectMateriales = document.getElementById("selectMateriales");
+    const seleccionadosMateriales = Array.from(selectMateriales.options)
+        .filter(opt => opt.selected)
+        .map(opt => opt.value);
+
+    //console.log("Materiales seleccionadas:", seleccionadosMateriales);
+
+    const selectMecanismos = document.getElementById("selectMecanismos");
+    const seleccionadosMecanismos = Array.from(selectMecanismos.options)
+        .filter(opt => opt.selected)
+        .map(opt => opt.value);
+
+    //console.log("Mecanismos seleccionadas:", seleccionadosMecanismos);
+
+    const minResistencia = document.getElementById("minResistencia");
+    const maxResistencia = document.getElementById("maxResistencia");
+    //console.log("Resistencia: min: ", minResistencia.value, ", max: ", maxResistencia.value);
+
+    let data = {
+        marcas: seleccionadosMarcas,
+        precio: { min: minPrecio.value, max: maxPrecio.value },
+        sexo: selectSexo.value,
+        diametro: { min: minDiametro.value, max: maxDiametro.value}, 
+        materiales: seleccionadosMateriales,
+        mecanismos: seleccionadosMecanismos,
+        resistencia_agua: { min: minResistencia.value, max: maxResistencia.value}
+    };
+
+    data = estandarizarData(data);
+
+    const jsonData = JSON.stringify(data);
+    console.log(jsonData);
+}
+
+function atribuirEscucharFiltrado() {
+    const selectMarcas = document.getElementById("selectMarcas");
+
+    const minPrecio = document.getElementById("minPrecio");
+    const maxPrecio = document.getElementById("maxPrecio");
+
+    const selectSexo = document.getElementById("selectSexo");
+
+    const minDiametro = document.getElementById("minDiametro");
+    const maxDiametro = document.getElementById("maxDiametro");
+
+    const selectMateriales = document.getElementById("selectMateriales");
+
+    const selectMecanismos = document.getElementById("selectMecanismos");
+
+    const minResistencia = document.getElementById("minResistencia");
+    const maxResistencia = document.getElementById("maxResistencia");
+
+    const elementos = [selectMarcas, minPrecio, maxPrecio, selectSexo, minDiametro, maxDiametro, selectMateriales, selectMecanismos, minResistencia, maxResistencia];
+    elementos.forEach((elemento) => {
+        elemento.addEventListener("change", () => {
+            escucharFiltrado();
+        })
+    })
+}
+
+
+cargarRelojes();
+atribuirEscucharFiltrado();
