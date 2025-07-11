5205853ce265378ae751197ca3cd8c16ae0b49fd
Juan-Dionesalvi <jdionesalvi@fi.uba.ar>
2025-07-11 11:43:05 -0300

    Los relojes se despliegan en index.html

---
+++ frontend/scripts/index.js
@@ -0,0 +1,34 @@
+async function cargarRelojes() {
+
+  return fetch(`http://localhost:3000/api/v1/relojes`)
+    .then((respuesta) => {
+        return respuesta.json();
+    })
+
+    .then((datos) => {
+        datos.forEach(infoReloj => {
+            const imagenReloj = document.createElement("img");
+            imagenReloj.src = infoReloj.imagen
+            imagenReloj.style.width = "128px";
+            imagenReloj.style.height = "150px";
+
+            const nombreReloj = document.createElement("h1");
+            nombreReloj.classList.add("title", "is-6")
+            nombreReloj.textContent = `${infoReloj.marca} ${infoReloj.nombre}`;
+
+            const nuevoReloj = document.createElement("div");
+            nuevoReloj.classList.add("nuevoReloj");
+
+            nuevoReloj.appendChild(imagenReloj);
+            nuevoReloj.appendChild(nombreReloj);
+
+            document.getElementById("contenedor_relojes").appendChild(nuevoReloj);
+        });
+    })
+
+    .catch((error) => {
+        console.error("Hubo un error al obtener los relojes: ", error);
+    });
+}
+
+cargarRelojes()
