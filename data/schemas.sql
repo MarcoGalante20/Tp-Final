d6269e0d3d96c2fa9dc1d99e74685ba19512e9cc
Marco Galante <mgalante@fi.uba.ar>
2025-07-11 12:27:09 -0300

    Elimina archivo viejo y agrega eliminación en cascada

--- data/schemas.sql
+++ data/schemas.sql
@@ -1,6 +1,6 @@
 create table relojes (
-	id serial primary key,
-	marca int not null REFERENCES marcas(id),
+	id_reloj serial primary key,
+	id_marca int REFERENCES marcas (id_marca) ON DELETE CASCADE,
 	nombre varchar(50) not null,
 	mecanismo varchar(50) not null default 'Cuarzo',
 	material varchar(50) not null default 'Acero-inox',
@@ -11,14 +11,14 @@ create table relojes (
 	sexo char
 );
 
-
 create table marcas (
-	id serial primary key,
-	imagen varchar(200) not null
+	id_marca serial primary key,
+	nombre varchar(20),
+	imagen varchar(200)
 );
 
 create table usuarios (
-	id serial primary key,
+	id_usuario serial primary key,
 	nombre varchar(50) not null,
 	contrasenia varchar(50) not null,
 	sexo char,
@@ -28,33 +28,33 @@ create table usuarios (
 
 
 create table resenias (
-	id serial primary key,
-	id_reloj int not null REFERENCES relojes (id),
-	id_usuario int not null REFERENCES usuarios (id),
+	id_resenia serial primary key,
+	id_reloj int REFERENCES relojes (id_reloj) ON DELETE CASCADE,
+	id_usuario int REFERENCES usuarios (id_usuario) ON DELETE CASCADE,
 	titulo varchar(50),
 	resenia text not null,
 	calificacion int not null,
 	fecha date not null,
 	meses_de_uso int not null
 );
- 
+
 
 create table relojes_usuarios (
-	id_usuario int not null REFERENCES usuarios (id),
-	id_reloj int not null REFERENCES relojes (id)
+	id_usuario int REFERENCES usuarios (id_usuario) ON DELETE CASCADE,
+	id_reloj int REFERENCES relojes (id_reloj) ON DELETE CASCADE
 );
 
 
 create table extras_reloj (
-	id_reloj int not null REFERENCES relojes (id),
+	id_reloj int REFERENCES relojes (id_reloj) ON DELETE CASCADE,
 	atributo varchar(50) not null
 );
 
+INSERT INTO marcas (nombre, imagen) VALUES
+	('Casio', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZWOJNh_QN9wy735YRBL7M7ZWQTNFMw0fl2g&s');
 
-INSERT INTO marcas(imagen) VALUES 
-	('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZWOJNh_QN9wy735YRBL7M7ZWQTNFMw0fl2g&s');
 
-INSERT INTO relojes (nombre, marca, mecanismo, material, resistencia_agua, diametro, precio, sexo) VALUES 
+INSERT INTO relojes (id_marca, nombre, mecanismo, material, resistencia_agua, diametro, precio, sexo) VALUES 
 	(1, 'Forester', 'Cuarzo', 'Plástico', 100, 38, 90000, 'M'), 
 	(1, 'Edifice EFV-100', 'Cuarzo', 'Acero inox', 100, 41, 170000, 'M');
 
@@ -68,6 +68,6 @@ INSERT INTO usuarios (nombre, contrasenia, sexo, edad, precio_buscado) VALUES
 	('Marco12', 'Boquitael+grande', 'M', 19, 120000),
 	('Juanfran', 'Calamardexvida', 'M', 19, 300000);
 
-INSERT INTO resenias (titulo, resenia, calificacion, fecha, meses_de_uso, id_reloj, id_usuario) VALUES 
+INSERT INTO resenias (id_reloj, id_usuario, titulo, resenia, calificacion, fecha, meses_de_uso) VALUES 
 	(1, 2, 'Un tanque en miniatura, impresionante', 'Su diseño simple pero atractivo, sumado a su estilo lo hacen un reloj resistente y muy recomendable', 5, '2025-05-12', 8),
 	(2, 1, 'El mejor para el día a día', 'Elegante, resistente, simple y no es caro. Qué más querés pedir?' 5, '2024-11-09', 7);
